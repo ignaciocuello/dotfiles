ceee05b8f116f78502dfcea890bed4a0a159a6ef
ignaciocuello <icuelloreyes97@gmail.com>
2024-03-17 14:27:15 +1100

    Add options from .vimrc to init.lua

--- .config/nvim/lua/ignacio/options.lua
+++ .config/nvim/lua/ignacio/options.lua
@@ -1,1 +1,12 @@
+-- use true colours
 vim.opt.termguicolors = true
+
+-- highlight search matches
+vim.opt.hlsearch = true
+-- highlight as you go
+vim.opt.incsearch = true
+
+-- enable line numbers
+vim.opt.number = true
+-- enable relative line numbers
+vim.opt.relativenumber = true
