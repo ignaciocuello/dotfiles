1d5ac49a3303221d0d5acff5fe37cca794b29703
ignaciocuello <icuelloreyes97@gmail.com>
2024-03-23 14:25:13 +1100

    Add lines to netrw

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,5 @@
 require("ignacio")
+-- TODO: find out what vim.g... is.move this to it's appropriate place
+-- TODO: move this to it's appropriate place.
+-- Adds line and relative line to netrw
+vim.g.netrw_bufsettings = 'noma nomod nu rnu nobl nowrap ro'
