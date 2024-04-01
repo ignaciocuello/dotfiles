d7fb1e0ba5feee805657cbcbd7bf64e63f365ce0
ignaciocuello <icuelloreyes97@gmail.com>
2024-04-01 11:32:02 +1100

    Use minpac over packer (for now at least).

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,3 +3,20 @@ require("ignacio")
 -- TODO: move this to it's appropriate place.
 -- Adds line and relative line to netrw
 vim.g.netrw_bufsettings = 'noma nomod nu rnu nobl nowrap ro'
+
+-- TODO: standardise double and single quotes
+-- Initialise minpac
+vim.cmd.packadd("minpac")
+vim.call("minpac#init")
+
+local add = vim.fn['minpac#add']
+add('k-takata/minpac', {type = 'opt'})
+
+-- Define user commands for updating/cleaning the plugins.
+-- Each of them calls PackInit() to load minpac and register
+-- the information of plugins, then performs the task.
+vim.cmd [[
+  command! PackUpdate call minpac#update()
+  command! PackClean call minpac#clean()
+  command! PackStatus call minpac#status()
+]]
