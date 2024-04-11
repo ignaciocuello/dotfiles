247a1c1f7938bd85a34550ae5f189f88a3c7e880
Ignacio Cuello <icuelloreyes97@gmail.com>
2024-04-12 08:36:14 +1000

    Setup ruby lsp for Neovim
    There were special instructions to follow for Neovim.
    From the README of ruby_lsp.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -603,7 +603,8 @@ require("lazy").setup({
 			})
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
-			require("mason-lspconfig").setup({
+			local masonLspConfig = require("mason-lspconfig")
+			masonLspConfig.setup({
 				handlers = {
 					function(server_name)
 						local server = servers[server_name] or {}
@@ -614,6 +615,18 @@ require("lazy").setup({
 						require("lspconfig")[server_name].setup(server)
 					end,
 				},
+				ensure_installed = vim.tbl_keys(servers),
+			})
+
+			masonLspConfig.setup_handlers({
+				function(server_name)
+					require("lspconfig")[server_name].setup({
+						capabilities = capabilities,
+						on_attach = on_attach,
+						settings = servers[server_name],
+						filetypes = (servers[server_name] or {}).filetypes,
+					})
+				end,
 			})
 		end,
 	},
@@ -894,5 +907,90 @@ require("lazy").setup({
 	},
 })
 
+-- textDocument/diagnostic support until 0.10.0 is released
+_timers = {}
+local function setup_diagnostics(client, buffer)
+	if require("vim.lsp.diagnostic")._enable then
+		return
+	end
+
+	local diagnostic_handler = function()
+		local params = vim.lsp.util.make_text_document_params(buffer)
+		client.request("textDocument/diagnostic", { textDocument = params }, function(err, result)
+			if err then
+				local err_msg = string.format("diagnostics error - %s", vim.inspect(err))
+				vim.lsp.log.error(err_msg)
+			end
+			local diagnostic_items = {}
+			if result then
+				diagnostic_items = result.items
+			end
+			vim.lsp.diagnostic.on_publish_diagnostics(
+				nil,
+				vim.tbl_extend("keep", params, { diagnostics = diagnostic_items }),
+				{ client_id = client.id }
+			)
+		end)
+	end
+
+	diagnostic_handler() -- to request diagnostics on buffer when first attaching
+
+	vim.api.nvim_buf_attach(buffer, false, {
+		on_lines = function()
+			if _timers[buffer] then
+				vim.fn.timer_stop(_timers[buffer])
+			end
+			_timers[buffer] = vim.fn.timer_start(200, diagnostic_handler)
+		end,
+		on_detach = function()
+			if _timers[buffer] then
+				vim.fn.timer_stop(_timers[buffer])
+			end
+		end,
+	})
+end
+
+-- adds ShowRubyDeps command to show dependencies in the quickfix list.
+-- add the `all` argument to show indirect dependencies as well
+local function add_ruby_deps_command(client, bufnr)
+	vim.api.nvim_buf_create_user_command(bufnr, "ShowRubyDeps", function(opts)
+		local params = vim.lsp.util.make_text_document_params()
+
+		local showAll = opts.args == "all"
+
+		client.request("rubyLsp/workspace/dependencies", params, function(error, result)
+			if error then
+				print("Error showing deps: " .. error)
+				return
+			end
+
+			local qf_list = {}
+			for _, item in ipairs(result) do
+				if showAll or item.dependency then
+					table.insert(qf_list, {
+						text = string.format("%s (%s) - %s", item.name, item.version, item.dependency),
+
+						filename = item.path,
+					})
+				end
+			end
+
+			vim.fn.setqflist(qf_list)
+			vim.cmd("copen")
+		end, bufnr)
+	end, {
+		nargs = "?",
+		complete = function()
+			return { "all" }
+		end,
+	})
+end
+
+require("lspconfig").ruby_ls.setup({
+	on_attach = function(client, buffer)
+		setup_diagnostics(client, buffer)
+		add_ruby_deps_command(client, buffer)
+	end,
+})
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
