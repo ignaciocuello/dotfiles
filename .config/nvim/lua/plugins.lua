f51d3eddecda673fe6388be5e4f06a9cb5d18b66
Ignacio Cuello <icuelloreyes97@gmail.com>
2024-05-01 21:12:14 +1000

    Add indent blankline

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -626,6 +626,11 @@ return {
 			vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 		end,
 	},
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		opts = {},
+	},
 	-- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
 	-- init.lua. If you want these files, they are in the repository, so you can just download them and
 	-- place them in the correct locations.
